d08aecda87152da96afe1722b7a51174b907c89d
chriszhu <gansocc@gmail.com>
2019-06-11 10:29:58 +1200

    floor plan box update

--- wp-content/themes/hotel-melbourne/home-floor-plan.php
+++ wp-content/themes/hotel-melbourne/home-floor-plan.php
@@ -31,10 +31,10 @@
         <div class="container">
             <div class="row">
                 <div class="tabs-section col-md-4">
-                    <ul class="nav nav-pills flex-column mobile-dropdown">
-                        <li class="nav-selector nav-item">
-                            <a href="javascript:void(0);" class="nav-link">Select Floor Plan</a>
-                        </li>
+                    <ul class="nav nav-pills flex-column">
+<!--                        <li class="nav-selector nav-item">-->
+<!--                            <a href="javascript:void(0);" class="nav-link">Select Floor Plan</a>-->
+<!--                        </li>-->
                         <li class="nav-item">
                             <a class="active nav-link" href="javascript:void();">Lilly</a>
                         </li>
@@ -57,6 +57,41 @@
                             <a class=" nav-link" href="javascript:void();" >Dogwood</a>
                         </li>
                     </ul>
+                </div>
+                <div class="tab-content col-md-8">
+                    <div class="floor-pln">
+                        <div class="blc-box">
+                            <h1>Lilly</h1>
+                            <p>90 M<sup>2</sup></p>
+                            <p>$700,000 - $800,000</p>
+                        </div>
+                        <ul class="floor-details">
+                            <li>
+                                <a href="javascript:void(0);" onclick="return false;">
+                                    <i class="icon-camera ">
+                                        <img class="lazyload loaded" data-src="https://2u0yuyu2fs02fa819ut739hy-wpengine.netdna-ssl.com/wp-content/themes/connorgroup-2019-understrap/img/bed.svg" alt="" src="https://2u0yuyu2fs02fa819ut739hy-wpengine.netdna-ssl.com/wp-content/themes/connorgroup-2019-understrap/img/bed.svg">
+                                    </i>
+                                    <span>1 bed</span>
+                                </a>
+                            </li>
+                            <li>
+                                <a href="javascript:void(0);" onclick="return false;">
+                                    <i class="icon-camrecorder">
+                                        <img class="lazyload loaded" data-src="https://2u0yuyu2fs02fa819ut739hy-wpengine.netdna-ssl.com/wp-content/themes/connorgroup-2019-understrap/img/bath.png" alt="" src="https://2u0yuyu2fs02fa819ut739hy-wpengine.netdna-ssl.com/wp-content/themes/connorgroup-2019-understrap/img/bath.png">
+                                    </i>
+                                    <span>1 bath</span>
+                                </a>
+                            </li>
+                            <li class="request-tour-btn">
+                                <a href="javascript:void(0);" onclick="return false;">
+                                    <i>
+                                        <img class="lazyload loaded" data-src="https://2u0yuyu2fs02fa819ut739hy-wpengine.netdna-ssl.com/wp-content/themes/connorgroup-2019-understrap/img/request-a-tour.png" alt="" src="https://2u0yuyu2fs02fa819ut739hy-wpengine.netdna-ssl.com/wp-content/themes/connorgroup-2019-understrap/img/request-a-tour.png" data-was-processed="true">
+                                    </i>
+                                    <span>Request Tour</span>
+                                </a>
+                            </li>
+                        </ul>
+                    </div>
                 </div>
             </div>
         </div>
