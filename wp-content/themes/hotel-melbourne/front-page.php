4b148fa1f8fc3e20d5730cb869cb325f03cdc427
chriszhu <gansocc@gmail.com>
2019-06-18 11:28:09 +1200

    floor plan section of front page update

--- wp-content/themes/hotel-melbourne/front-page.php
+++ wp-content/themes/hotel-melbourne/front-page.php
@@ -1,28 +1,33 @@
 <?php
-$melbourne_option=hotel_melbourne_theme_default_data(); 
-$front_page_options = wp_parse_args(  get_option( 'melbourne_option', array() ), $melbourne_option );
-if ($front_page_options['front_page_enabled']=="1" && is_front_page()) {
+    $melbourne_option = hotel_melbourne_theme_default_data();
+    $front_page_options = wp_parse_args(get_option('melbourne_option', array()), $melbourne_option);
+    if ($front_page_options['front_page_enabled'] == "1" && is_front_page()) {
 
-get_header();
+        get_header();
 
-	/****** Home Slider ******/
-	get_template_part('home','slider');
+        /****** Home Slider ******/
+        get_template_part('home', 'slider');
 
-    /****** Home floor plan ******/
-    get_template_part('home','floor-plan');
-			  
-	/****** Home colout ******/
-	get_template_part('home','service');
+        /****** Home floor plan ******/
+        get_template_part('home', 'floor-plan');
 
-	/****** Home gallery ******/
-	get_template_part('home','gallery');
-	 
-	/****** Home blog ******/
-	get_template_part('home','blog');
+        /****** Home blog ******/
+        get_template_part('home', 'blog');
 
-get_footer();
-} else {
-		if(is_page())
-		{ get_template_part('page'); } 
-		else { get_template_part('index'); } }
+        /****** Home colout ******/
+        get_template_part('home', 'service');
+
+        /****** Home gallery ******/
+        get_template_part('home', 'gallery');
+
+
+        get_footer();
+    }
+    else {
+        if (is_page()) {
+            get_template_part('page');
+        } else {
+            get_template_part('index');
+        }
+    }
 ?>
--- wp-content/themes/hotel-melbourne/home-floor-plan.php
+++ wp-content/themes/hotel-melbourne/home-floor-plan.php
@@ -12,16 +12,16 @@
     <div class="floor-nav">
         <div class="container">
             <ul class="nav nav-pills" role="tablist">
-                <li class="nav-item">
-                    <a href="javascript:void(0);" class="nav-link active">All</a>
+                <li class="nav-item"  data-bed-num="all">
+                    <a href="javascript:void(0);" class="nav-link active" >All</a>
                 </li>
-                <li class="nav-item">
+                <li class="nav-item" data-bed-num="1">
                     <a href="javascript:void(0);" class="nav-link" >1 Bed</a>
                 </li>
-                <li class="nav-item">
+                <li class="nav-item" data-bed-num="2">
                     <a href="javascript:void(0);" class="nav-link">2 Bed</a>
                 </li>
-                <li class="nav-item">
+                <li class="nav-item" data-bed-num="3">
                     <a href="javascript:void(0);" class="nav-link">3+ Bed</a>
                 </li>
             </ul>
@@ -30,40 +30,40 @@
     <div class="floor-content">
         <div class="container">
             <div class="row">
-                <div class="tabs-section col-md-4">
+                <div class="tabs-section col-md-3">
                     <ul class="nav nav-pills flex-column">
 <!--                        <li class="nav-selector nav-item">-->
 <!--                            <a href="javascript:void(0);" class="nav-link">Select Floor Plan</a>-->
 <!--                        </li>-->
                         <li class="nav-item">
-                            <a class="active nav-link" href="javascript:void();">Lilly</a>
+                            <a class="active nav-link" href="javascript:" data-unit-id="0">Lilly</a>
                         </li>
                         <li class="nav-item">
-                            <a class=" nav-link" href="javascript:void();">Azalea</a>
+                            <a class=" nav-link" href="javascript:" data-unit-id="1">Azalea</a>
                         </li>
                         <li class="nav-item">
-                            <a class=" nav-link" href="javascript:void();" >Gardenia</a>
+                            <a class=" nav-link" href="javascript:" data-unit-id="2">Gardenia</a>
                         </li>
                         <li class="nav-item">
-                            <a class=" nav-link" href="javascript:void();" >Sunflower</a>
+                            <a class=" nav-link" href="javascript:" data-unit-id="3">Sunflower</a>
                         </li>
                         <li class="nav-item">
-                            <a class=" nav-link" href="javascript:void();">Magnolia</a>
+                            <a class=" nav-link" href="javascript:" data-unit-id="4">Magnolia</a>
                         </li>
                         <li class="nav-item">
-                            <a class=" nav-link" href="javascript:void();">Camelia</a>
+                            <a class=" nav-link" href="javascript:" data-unit-id="5">Camelia</a>
                         </li>
                         <li class="nav-item">
-                            <a class=" nav-link" href="javascript:void();" >Dogwood</a>
+                            <a class=" nav-link" href="javascript:" data-unit-id="6">Dogwood</a>
                         </li>
                     </ul>
                 </div>
-                <div class="tab-content col-md-8">
+                <div class="tab-content col-md-9">
                     <div class="floor-pln">
                         <div class="blc-box">
-                            <h1>Lilly</h1>
-                            <p>90 M<sup>2</sup></p>
-                            <p>$700,000 - $800,000</p>
+                            <h1 id="unit-name">Lilly</h1>
+                            <p><span id="unit-area">90</span> M<sup>2</sup></p>
+                            <p id="unit-price">$700,000 - $800,000</p>
                         </div>
                         <ul class="floor-details">
                             <li>
@@ -71,7 +71,7 @@
                                     <i class="icon-camera ">
                                         <img class="lazyload loaded" data-src="https://2u0yuyu2fs02fa819ut739hy-wpengine.netdna-ssl.com/wp-content/themes/connorgroup-2019-understrap/img/bed.svg" alt="" src="https://2u0yuyu2fs02fa819ut739hy-wpengine.netdna-ssl.com/wp-content/themes/connorgroup-2019-understrap/img/bed.svg">
                                     </i>
-                                    <span>1 bed</span>
+                                    <span id="unit-bed">1 bed</span>
                                 </a>
                             </li>
                             <li>
@@ -79,7 +79,7 @@
                                     <i class="icon-camrecorder">
                                         <img class="lazyload loaded" data-src="https://2u0yuyu2fs02fa819ut739hy-wpengine.netdna-ssl.com/wp-content/themes/connorgroup-2019-understrap/img/bath.png" alt="" src="https://2u0yuyu2fs02fa819ut739hy-wpengine.netdna-ssl.com/wp-content/themes/connorgroup-2019-understrap/img/bath.png">
                                     </i>
-                                    <span>1 bath</span>
+                                    <span id="unit-bath">1 bath</span>
                                 </a>
                             </li>
                             <li class="request-tour-btn">
@@ -92,8 +92,124 @@
                             </li>
                         </ul>
                     </div>
+                    <div class="floor-pln-img">
+                        <img alt="floor-pln-img" id="unit-floor-pln-img" src="/wp-content/uploads/2019/06/1bed1bath-1.jpg" />
+                    </div>
                 </div>
             </div>
         </div>
     </div>
 </section>
+<script type="application/javascript">
+    var floorPlnComponent = {
+        state: {
+            actived: 0,
+            units: [
+                {
+                    roomName: 'Lilly',
+                    bed: 1,
+                    bath: 1,
+                    area: 80,
+                    price: '$800,000 - $900,000',
+                    floorPlnImg: '/wp-content/uploads/2019/06/1bed1bath-1.jpg'
+                },
+                {
+                    roomName: 'Azalea',
+                    bed: 2,
+                    bath: 1,
+                    area: 90,
+                    price: '$800,000 - $900,000',
+                    floorPlnImg: '/wp-content/uploads/2019/06/1bed1bath-2.jpg'
+                },
+                {
+                    roomName: 'Gardenia',
+                    bed: 2,
+                    bath: 1,
+                    area: 95,
+                    price: '$820,000 - $950,000',
+                    floorPlnImg: '/wp-content/uploads/2019/06/1bed1bath-2.jpg'
+                },
+                {
+                    roomName: 'Sunflower',
+                    bed: 2,
+                    bath: 2,
+                    area: 95,
+                    price: '$850,000 - $1,000,000',
+                    floorPlnImg: '/wp-content/uploads/2019/06/2bed2bath-2.jpg'
+                },
+                {
+                    roomName: 'Magnolia',
+                    bed: 2,
+                    bath: 2,
+                    area: 100,
+                    price: '$950,000 - $1,000,000',
+                    floorPlnImg: '/wp-content/uploads/2019/06/2bed2bath-2.jpg'
+                },
+                {
+                    roomName: 'Camelia',
+                    bed: 3,
+                    bath: 2,
+                    area: 105,
+                    price: '$900,000 - $1,100,000',
+                    floorPlnImg: '/wp-content/uploads/2019/06/3bed2bath.jpg'
+                },
+                {
+                    roomName: 'Dogwood',
+                    bed: 3,
+                    bath: 2,
+                    area: 120,
+                    price: '$1,000,000 - $1,200,000',
+                    floorPlnImg: '/wp-content/uploads/2019/06/3bed2.5bath.jpg'
+                }
+            ]
+        },
+        init: function () {
+            
+        },
+        load: function () {
+            this.init();
+            this.navHandleClick();
+            this.tabsHandleClick();
+        },
+        navHandleClick: function () {
+            let _this = this;
+            $(".floor-nav .nav-item").on('click', function () {
+                let bedNum = $(this).data("bed-num");
+                let i = 0;
+                if ( bedNum === 'all' )
+                    $(".tabs-section .nav-link").show();
+                else
+                    $(_this.state.units).each(function (key, item) {
+                        bedNum === item.bed  ? $(".tabs-section .nav-link").eq(key).show() : $(".tabs-section .nav-link").eq(key).hide();
+                    });
+            });
+        },
+        tabsHandleClick: function () {
+            let _this = this;
+            $(".tabs-section .nav-link").on('click', function () {
+                let unitId = Number($(this).attr('data-unit-id'));
+                let unit = _this.state.units[unitId];
+                _this.state.actived = unitId;
+                _this.tabSwitch(unitId);
+                $(".tab-content").fadeOut(800, function () {
+                    $("#unit-name").html(unit.roomName);
+                    $("#unit-area").html(unit.area);
+                    $("#unit-bed").html(unit.bed + "&nbsp;bed");
+                    $("#unit-bath").html(unit.bath + "&nbsp;bath");
+                    $("#unit-price").html(unit.price);
+                    $("#unit-floor-pln-img").attr("src", unit.floorPlnImg);
+                    $(".tab-content").fadeIn(1200);
+                });
+            });
+        },
+        tabSwitch: function (unitId) {
+            $(".tabs-section .nav-link.active").removeClass("active");
+            $(".tabs-section .nav-item").eq(unitId).find(".nav-link").addClass("active");
+        }
+    };
+
+    $(function() {
+        // Handler for .ready() called.
+        floorPlnComponent.load();
+    });
+</script>
